3da2fe9c78963847b7f958d6c6114aa7385eb1e7
carlosctu <carlosctu94@gmail.com>
2022-06-29 19:37:02 -0300

    [feat] AXIOS Post Quizz Basic Info

--- scripts.js
+++ scripts.js
@@ -1,14 +1,95 @@
-
-
-
-
 /* você aqui */
 
+// Criação do Quizz
 
-
-
-
-
+// Enviando info básica para o AXIOS
+function sendBasicInfo() {
+  const title = document.querySelector(".basic-info-title").value;
+  const image = document.querySelector(".basic-info-image").value;
+  const info = {
+    title: title,
+    image: image,
+    questions: [
+		{
+			title: "Título da pergunta 1",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 2",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 3",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		}
+	],
+	levels: [
+		{
+			title: "Título do nível 1",
+			image: "https://http.cat/411.jpg",
+			text: "Descrição do nível 1",
+			minValue: 0
+		},
+		{
+			title: "Título do nível 2",
+			image: "https://http.cat/412.jpg",
+			text: "Descrição do nível 2",
+			minValue: 50
+		}
+	]
+    }
+//   };
+  console.log(info);
+  // Post: post("endpoint da API", dados -> neste caso seria o "info")
+  const promisse = axios.post(
+    "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes",
+    info
+  );
+  promisse.then((response) => {
+    const title = response.data.title;
+    const image = response.data.image;
+    console.log(title, image);
+  });
+  promisse.catch(() =>
+    console.log("Deu um erro no envio dos dados de Info Basicas")
+  );
+}
 
 /* 
 
@@ -21,12 +102,5 @@ Aí a partir daqui sou eu com o display e listagem, e antes seria você com a cr
 ------------------------------------------------------------------------------------------------------------
 
 */
-
-
-
-
-
-
-
 
 /* eu aqui */
