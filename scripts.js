666a5e26271aeb253ac517c469d869ba9f062652
Leonardo de Souza Ferreira <leonardosouzaf@icloud.com>
2022-06-29 17:09:49 -0300

    header-done

---
+++ scripts.js
@@ -0,0 +1,32 @@
+
+
+
+
+/* você aqui */
+
+
+
+
+
+
+
+/* 
+
+------------------------------------------------------------------------------------------------------------
+
+Podemos separar somente o código do JS com esse comentário, pode ficar mais organizado para cada um de nós.
+Mas depois quando formos entregar, a gente retira esse comentário aqui.  
+Aí a partir daqui sou eu com o display e listagem, e antes seria você com a criação do quizz.
+
+------------------------------------------------------------------------------------------------------------
+
+*/
+
+
+
+
+
+
+
+
+/* eu aqui */
