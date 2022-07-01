ea5bcb2a977b1c77ad0915ebdff0206d3f92e6fd
carlosctu <carlosctu94@gmail.com>
2022-06-30 23:32:08 -0300

    [change] Tela 3.1 JS

--- scripts.js
+++ scripts.js
@@ -1,105 +1,108 @@
 /* você aqui */
-
 // Criação do Quizz
-
-// Tela 3.1 - Info básica do Quiz 
-function sendBasicInfo() {
-  const title = document.querySelector(".basic-info-title").value;
-  const image = document.querySelector(".basic-info-image").value;
-  const info = {
-    title: title,
-    image: image,
-    questions: [
-      {
-        title: "Título da pergunta 1",
-        color: "#123456",
-        answers: [
-          {
-            text: "Texto da resposta 1",
-            image: "https://http.cat/411.jpg",
-            isCorrectAnswer: true,
-          },
-          {
-            text: "Texto da resposta 2",
-            image: "https://http.cat/412.jpg",
-            isCorrectAnswer: false,
-          },
-        ],
-      },
-      {
-        title: "Título da pergunta 2",
-        color: "#123456",
-        answers: [
-          {
-            text: "Texto da resposta 1",
-            image: "https://http.cat/411.jpg",
-            isCorrectAnswer: true,
-          },
-          {
-            text: "Texto da resposta 2",
-            image: "https://http.cat/412.jpg",
-            isCorrectAnswer: false,
-          },
-        ],
-      },
-      {
-        title: "Título da pergunta 3",
-        color: "#123456",
-        answers: [
-          {
-            text: "Texto da resposta 1",
-            image: "https://http.cat/411.jpg",
-            isCorrectAnswer: true,
-          },
-          {
-            text: "Texto da resposta 2",
-            image: "https://http.cat/412.jpg",
-            isCorrectAnswer: false,
-          },
-        ],
-      },
-    ],
-    levels: [
-      {
-        title: "Título do nível 1",
-        image: "https://http.cat/411.jpg",
-        text: "Descrição do nível 1",
-        minValue: 0,
-      },
-      {
-        title: "Título do nível 2",
-        image: "https://http.cat/412.jpg",
-        text: "Descrição do nível 2",
-        minValue: 50,
-      },
-    ],
-  };
-
-  // Post: post("endpoint do AXIOS", dados -> neste caso seria o "info")
-  const promisse = axios.post(
-    "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes",
-    info
-  );
-  promisse.then((response) => {
-    const title = response.data.title;
-    const image = response.data.image;
-    console.log(title, image);
-  });
-  promisse.catch(() =>
-    console.log("Deu um erro no envio dos dados de Info Basicas")
-  );
+// Navegação entre páginas
+function nextPage(element) {
+  console.log("caiu aqui")
+  console.log(element);
+  const section = element.parentNode.classList;
+  element.parentNode.classList.add("hidden");
+  if (section.contains("basic-info")) {
+    document.querySelector(".quiz-questions").classList.remove("hidden");
+  } else if (section.contains("quiz-questions")) {
+    document.querySelector(".quiz-levels").classList.remove("hidden");
+  } else if (section.contains("quiz-levels")) {
+    document.querySelector(".quiz-success").classList.remove("hidden");
+  }
 }
+
+// Tela 3.1 - Info básica do Quiz
+// function sendBasicInfo() {
+//   const title = document.querySelector(".basic-info-title").value;
+//   const image = document.querySelector(".basic-info-image").value;
+//   const info = {
+//     title: title,
+//     image: image,
+//     questions: [
+//       {
+//         title: "Título da pergunta 1",
+//         color: "#123456",
+//         answers: [
+//           {
+//             text: "Texto da resposta 1",
+//             image: "https://http.cat/411.jpg",
+//             isCorrectAnswer: true,
+//           },
+//           {
+//             text: "Texto da resposta 2",
+//             image: "https://http.cat/412.jpg",
+//             isCorrectAnswer: false,
+//           },
+//         ],
+//       },
+//       {
+//         title: "Título da pergunta 2",
+//         color: "#123456",
+//         answers: [
+//           {
+//             text: "Texto da resposta 1",
+//             image: "https://http.cat/411.jpg",
+//             isCorrectAnswer: true,
+//           },
+//           {
+//             text: "Texto da resposta 2",
+//             image: "https://http.cat/412.jpg",
+//             isCorrectAnswer: false,
+//           },
+//         ],
+//       },
+//       {
+//         title: "Título da pergunta 3",
+//         color: "#123456",
+//         answers: [
+//           {
+//             text: "Texto da resposta 1",
+//             image: "https://http.cat/411.jpg",
+//             isCorrectAnswer: true,
+//           },
+//           {
+//             text: "Texto da resposta 2",
+//             image: "https://http.cat/412.jpg",
+//             isCorrectAnswer: false,
+//           },
+//         ],
+//       },
+//     ],
+//     levels: [
+//       {
+//         title: "Título do nível 1",
+//         image: "https://http.cat/411.jpg",
+//         text: "Descrição do nível 1",
+//         minValue: 0,
+//       },
+//       {
+//         title: "Título do nível 2",
+//         image: "https://http.cat/412.jpg",
+//         text: "Descrição do nível 2",
+//         minValue: 50,
+//       },
+//     ],
+//   };
+
+//   // Post: post("endpoint do AXIOS", dados -> neste caso seria o "info")
+//   const promisse = axios.post(
+//     "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes",
+//     info
+//   );
+//   promisse.then((response) => {
+//     const title = response.data.title;
+//     const image = response.data.image;
+//     console.log(title, image);
+//   });
+//   promisse.catch(() =>
+//     console.log("Deu um erro no envio dos dados de Info Basicas")
+//   );
+// }
 // Tela 3.2 - Perguntas do Quiz
-
-
-
-
-
-
-
-
-
-
 
 /* 
 
@@ -113,15 +116,15 @@ Aí a partir daqui sou eu com o display e listagem, e antes seria você com a cr
 
 */
 
-
 /* Tela 1 Lista de Quizzes */
 
 let content;
-let promisseGetQuizzes = axios.get('https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes');
+let promisseGetQuizzes = axios.get(
+  "https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes"
+);
 promisseGetQuizzes.then(displayTheQuizzes);
 
-function displayTheQuizzes(answer){
-
+function displayTheQuizzes(answer) {
   //const userQuizzesSerialized = localStorage.getItem("userQuizzes");
   //const userQuizzes = JSON.parse(userQuizzesSerialized);
 
@@ -129,18 +132,16 @@ function displayTheQuizzes(answer){
   let quizzExample = {
     id: 1,
     title: "Título do quizz",
-    image: "https://http.cat/411.jpg"
-  }
+    image: "https://http.cat/411.jpg",
+  };
 
   const userQuizzes = [quizzExample]; //array de objetos, sendo cada objeto um quizz
 
-
-  
   let quizzes = answer.data;
-  content = document.querySelector('.content');
+  content = document.querySelector(".content");
 
-  if(userQuizzes.length === 0){
-    content.innerHTML=`
+  if (userQuizzes.length === 0) {
+    content.innerHTML = `
     <div class="display-lists">
     <div class="user-none-quizzes">
       <div class="none-quizzes-alert">
@@ -152,18 +153,18 @@ function displayTheQuizzes(answer){
       <div class="title-all-quizzes">Todos os Quizzes</div>
       <div class="all-quizzes-list"></div>
     </div>
-    `
+    `;
 
-    let displayAllList = document.querySelector('.all-quizzes-list');
-    for(let i = 0; i<quizzes.length; i++){
-      displayAllList.innerHTML+=`
+    let displayAllList = document.querySelector(".all-quizzes-list");
+    for (let i = 0; i < quizzes.length; i++) {
+      displayAllList.innerHTML += `
       <div class="quizz"><img src="${quizzes[i].image}" class="quizz-image"><div class="quizz-image-gradient"></div><p>${quizzes[i].title}</p></div>
       `;
     }
   }
 
-  if(userQuizzes.length !== 0){
-    content.innerHTML=`
+  if (userQuizzes.length !== 0) {
+    content.innerHTML = `
     <div class="user-quizzes">
       <div class="title-user-quizzes">
         Seus Quizzes <ion-icon name="add-circle"></ion-icon>
@@ -174,19 +175,18 @@ function displayTheQuizzes(answer){
       <div class="title-all-quizzes">Todos os Quizzes</div>
       <div class="all-quizzes-list"></div>
     </div>
-    `
+    `;
 
-    let displayUserList = document.querySelector('.user-quizzes-list');
-    for(let i = 0; i<userQuizzes.length; i++){
-      displayUserList.innerHTML+=`
+    let displayUserList = document.querySelector(".user-quizzes-list");
+    for (let i = 0; i < userQuizzes.length; i++) {
+      displayUserList.innerHTML += `
       <div class="quizz"><img src="${userQuizzes[i].image}" class="quizz-image"><div class="quizz-image-gradient"></div><p>${userQuizzes[i].title}</p></div>
       `;
     }
 
-
-    let displayAllList = document.querySelector('.all-quizzes-list');
-    for(let i = 0; i<quizzes.length; i++){
-      displayAllList.innerHTML+=`
+    let displayAllList = document.querySelector(".all-quizzes-list");
+    for (let i = 0; i < quizzes.length; i++) {
+      displayAllList.innerHTML += `
       <div class="quizz"><img src="${quizzes[i].image}" class="quizz-image"><div class="quizz-image-gradient"></div><p>${quizzes[i].title}</p></div>
       `;
     }
